4bd7547ffbb96edacdc4768c954ae0fa9b4f655e
zahimizrahi <zahimizrahi1@gmail.com>
2020-05-02 15:59:19 +0300

    ADDED: added model and demo

---
+++ Model.py
@@ -0,0 +1,300 @@
+import time
+import numpy as np
+import time
+import datetime
+from tensorflow.keras.layers import Conv2D, Input, MaxPooling2D, Lambda, Flatten, Dense
+# from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.models import Sequential, Model, load_model
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras import backend as K
+from tensorflow.keras.optimizers import Adam, SGD, RMSprop
+from tensorflow.keras.metrics import Precision, Recall
+from tensorflow.keras.callbacks import TensorBoard, EarlyStopping, ModelCheckpoint
+from tensorboard.plugins.hparams import api as hyper
+from tensorflow.random import set_seed
+from numpy.random import seed
+from skimage.transform import resize
+from skimage import io
+import matplotlib.pyplot as plt
+from utils import get_image_name, init_tensor_data, abs_distance
+
+
+def initialize_bias(shape, name=None, dtype=None):
+    """
+    initialize the weights of the biases for the network
+    :param shape: the shape of the bias
+    :param name: name of the weights
+    :param dtype: type of the weights
+    :return: the returned value by the normal distribution
+    """
+    return np.random.normal(loc=0.5, scale=1e-2, size=shape)
+
+
+def initialize_weights(shape, name=None, dtype=None):
+    """
+    initialize the weights for the network
+    :param shape: the shape of the weights
+    :param name: name of the weights
+    :param dtype: type of the weights
+    :return: the returned value by the normal distribution
+    """
+    return np.random.normal(loc=0.0, scale=1e-2, size=shape)
+
+
+def initialize_weights_dense(shape, name=None, dtype=None):
+    """
+    initialize weights for the dense layers
+    :param shape: the shape of the weights
+    :param name: name of the weights
+    :param dtype: type of the weights
+    :return: the returned value by the normal distribution
+    """
+    return np.random.normal(loc=0.0, scale=0.2, size=shape)
+
+
+class SiameseModel:
+
+    def __init__(self, input_shape=(250, 250, 1), num_layers=4, filter_size=64,
+                 kernel_init=initialize_weights, kernel_init_dense=initialize_weights_dense, kernel_reg=l2(3e-4),
+                 kernel_reg_dense=l2(1e-3), bias_init=initialize_bias,
+                 kernel_sizes=[(10, 10), (7, 7), (4, 4), (4, 4)], units=4 * 64, optimizer='adam', lr=3e-4,
+                 loss='binary_crossentropy', metrics=['accuracy', Precision(name='Precision'), Recall(name='Recall')],
+                 pretrained_weights=None, model_path=None, distance=abs_distance, distance_output_shape=None,
+                 activation_predict='sigmoid'):
+        """
+        implement the siamese model as defined in the article - Siamese Neural Network for One-Shot Image Recognition
+        :param input_shape: the shape of the images
+        :param num_layers: number of layers
+        :param filter_size: the size of filter
+        :param kernel_init: the function for initializing the weights
+        :param kernel_init_dense: the function for initializing the weights for dense layer
+        :param kernel_reg: the regularization function for all the layer but the Dense
+        :param kernel_reg_dense: the regulatization fucntion for Dense layer
+        :param bias_init: the function for initialization the biases.
+        :param kernel_sizes: the size of the hidden layers
+        :param units: the number of units in Dense layer
+        :param optimizer: the kind of optimizer
+        :param loss: the kind of loss function
+        :param metrics: the metrics we'll use
+        :param pretrained_weights: if given, it will contain the pretrained weights of the model. else None
+        :param model_path: if given, it will save time and just load the model from model path instead of building it. else None
+        :param distance: the distance function for evaluating the distance
+        :param distance_output_shape: the size of the ouput of distance function
+        :param activation_predict: the kind of the activation in the prediction.
+        """
+        if model_path is not None:
+            self.model = load_model(model_path)
+
+        # TODO: implement distance
+
+        # define the model
+        model = Sequential()
+
+        # define the tensors for the input images
+        left_image = Input(input_shape)
+        right_image = Input(input_shape)
+
+        self.pretrained_weights = pretrained_weights
+        self.model_path = model_path
+        self.distance = distance
+        self.distance_output_shape = distance_output_shape
+        self.kernel_init = kernel_init
+        self.kernel_init_dense = kernel_init_dense
+        self.kernel_reg = kernel_reg
+        self.kernel_reg_dense = kernel_reg_dense
+        self.bias_init = bias_init
+        self.kernel_sizes = kernel_sizes
+        self.optimizer = optimizer
+        self.activation_predict = activation_predict
+        self.loss = loss
+        self.metrics = metrics
+        self.filter_size = filter_size
+        self.units = units
+        self.lr = lr
+
+        if optimizer is None or optimizer == 'adam':
+            self.optimizer = Adam(lr=lr)
+        elif optimizer == 'sgd':
+            self.optimizer = SGD(lr=lr)
+        elif optimizer == 'rmd':
+            self.optimizer = RMSprop(lr=lr)
+
+        self.optimizer_name = optimizer
+
+        # define the layers of the network
+        for i in range(num_layers - 1):
+            model.add(Conv2D(
+                filters=(filter_size * (2 ** i)),
+                kernel_size=kernel_sizes[i],
+                activation='relu',
+                input_shape=input_shape,
+                kernel_initializer=kernel_init,
+                kernel_regularizer=kernel_reg
+            )
+            )
+            model.add(MaxPooling2D())
+
+        model.add(Conv2D(
+            filters=(filter_size * 8),
+            kernel_size=kernel_sizes[3],
+            activation='relu',
+            input_shape=input_shape,
+            kernel_initializer=initialize_weights,
+            kernel_regularizer=kernel_reg
+        ))
+
+        model.add(Flatten())
+
+        model.add(Dense(
+            units=units,
+            activation=activation_predict,
+            kernel_initializer=kernel_init_dense,
+            kernel_regularizer=kernel_reg_dense,
+            bias_initializer=bias_init
+        )
+        )
+
+        encoded_left = model(left_image)
+        encoded_right = model(right_image)
+
+        # add a customized layer to compute the absolute distances between the left and right encodings
+        distance_func = Lambda(distance, distance_output_shape)([encoded_left, encoded_right])
+
+        # add a dense layer with sigmoid as activation to generate the similarity score
+        prediction = Dense(1, activation=activation_predict, bias_initializer=bias_init)(distance_func)
+        # prediction = distance_func
+
+        siamese_model = Model(inputs=[left_image, right_image], outputs=prediction)
+        siamese_model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
+        # siamese_model.summary()
+
+        if self.pretrained_weights:
+            siamese_model.load_weights(pretrained_weights)
+
+        self.model = siamese_model
+
+    def fit(self, train_paths_labels, val_paths_labels, table=None, _resize=[250, 250],
+            norm=255.0, batch_size=128, epochs=30, verbose=True, train_data=None, validation_data=None,
+            callbacks=None, steps_per_epoch=None, validation_steps=None, hparam=None, prefix='', patience=3,
+            tensorboard_hist_freq=1):
+        start_time = time.time()
+        #self.input_shape = (_resize[0], _resize[1], 1)
+        start_runtime = datetime.datetime.now().strftime('%m%d-%H%M%S')
+        log_name = f'{prefix}_{start_runtime}_shape{_resize[0]}_batch{batch_size}_epochs{epochs}_lr{self.lr}'
+        log_paths = f'fit_logs/{log_name}'
+
+        if table is not None:
+            # table = PrettyTable['Start_Runtime', 'Name', 'Resize', 'Epochs', 'Units', 'Num_Filters', 'Optimizer', 'LR', 'Batch Size'])
+            if hparam is not None:
+                table.add_row(
+                    [start_runtime, log_name, _resize[0], epochs] + [hparam[param] for param in hparam])
+            else:
+                table.add_row(
+                    [start_runtime, log_name, _resize[0], epochs, self.units, self.filter_size,
+                     batch_size, self.optimizer_name, self.lr])
+            print(table)
+
+        if steps_per_epoch is None:
+            steps_per_epoch = len(train_paths_labels) // batch_size
+        if validation_steps is None:
+            validation_steps = len(val_paths_labels) // batch_size
+
+        print(f'Steps per epoch: {steps_per_epoch}')
+        print(f'Validation Steps:{validation_steps}')
+
+        train_data = init_tensor_data(train_data, images_labels_path=train_paths_labels, norm=norm, _resize=_resize,
+                                      batch_size=batch_size, verbose=verbose)
+        validation_data = init_tensor_data(validation_data, images_labels_path=val_paths_labels, norm=norm,
+                                           _resize=_resize, batch_size=batch_size, verbose=verbose)
+        if self.pretrained_weights is None:
+            if callbacks is None:
+                callbacks = []
+
+            tb_callback = TensorBoard(
+                log_dir=log_paths,
+                histogram_freq=tensorboard_hist_freq,
+            )
+            early_stop = EarlyStopping(patience=patience, verbose=verbose)
+            mc = ModelCheckpoint(f'{log_name}.h5', verbose=0, save_best_only=True, save_weights_only=True)
+
+            callbacks.append(tb_callback)
+            callbacks.append(early_stop)
+            callbacks.append(mc)
+
+            if hparam is not None:
+                callbacks.append(hyper.KerasCallback(log_paths, hparam))
+
+            self.model.fit(train_data, epochs=epochs, verbose=verbose, callbacks=callbacks,
+                           validation_data=validation_data, steps_per_epoch=steps_per_epoch,
+                           validation_steps=validation_steps)
+
+            # model.save_weights(f'{log_name}.h5')
+        train_time = time.time() - start_time
+        print(f'############## {train_time:.2f} seconds! ##############')
+        return table, train_time
+
+    def evaluate(self, image_labels_path, data=None, steps=None, norm=255.0, _resize=[250, 250], verbose=True):
+        data = init_tensor_data(data, images_labels_path=image_labels_path, norm=norm, _resize=_resize, batch_size=1)
+        if steps is None:
+            steps = len(image_labels_path)
+        return self.model.evaluate(data, steps=steps, verbose=True)
+
+    def predict(self, images_labels_path, data=None, steps=None, norm=255.0, _resize=[250, 250], images_to_print=0,
+                verbose=True):
+        data = init_tensor_data(data, images_labels_path=images_labels_path, norm=norm, _resize=_resize, batch_size=1)
+        if steps is None:
+            steps = len(images_labels_path)
+        preds = np.squeeze(self.model.predict(data, steps=steps, verbose=verbose))
+        print(f'Shape of prediction verctor: {preds.shape}')
+
+        split = 1
+        num_of_split = 2
+        plt.figure(figsize=(num_of_split * 4, images_to_print * 4))
+
+        for i in range(images_to_print):
+            left_img, right_img, y_true = images_labels_path[i]
+            y_pred = preds[i]
+            plt.subplot(images_to_print, num_of_split, split)
+            plt.imshow(resize(io.imread(left_img, as_gray=True), (_resize[0], _resize[1])), cmap='gray', vmin=0, vmax=1)
+            plt.title(f'{i}: {get_image_name(left_img)}\n true={y_true}\npred={y_pred:.2f}')
+            plt.grid(False)
+            plt.xticks([])
+            plt.yticks([])
+
+            plt.subplot(images_to_print, num_of_split, split)
+            plt.imshow(resize(io.imread(right_img, as_gray=True), (_resize[0], _resize[1])), cmap='gray', vmin=0, vmax=1)
+            plt.title(f'{i}: {get_image_name(right_img)}\n true={y_true}\npred={y_pred:.2f}')
+            plt.grid(False)
+            plt.xticks([])
+            plt.yticks([])
+
+            split += num_of_split
+        plt.show()
+        return preds
+
+    def fit_evaluate(self, train_paths_labels, val_paths_labels, test_paths_labels, fit_table=None, eval_table=None,
+                     _resize=[250, 250], norm=255.0, batch_size=128, epochs=30, verbose=True,
+                     train_data=None,
+                     validation_data=None,
+                     callbacks=None, steps_per_epoch=None, validation_steps=None, prefix='', patience=3,
+                     tensorboard_hist_freq=1, random_seed=42):
+        seed(random_seed)
+        set_seed(random_seed)
+        _, train_time = self.fit(train_paths_labels=train_paths_labels, val_paths_labels=val_paths_labels,
+                                 table=fit_table,
+                                 _resize=_resize, norm=norm, batch_size=batch_size, epochs=epochs,
+                                 verbose=verbose, train_data=train_data, validation_data=validation_data,
+                                 callbacks=callbacks,
+                                 steps_per_epoch=steps_per_epoch, validation_steps=validation_steps, prefix=prefix,
+                                 patience=patience,
+                                 tensorboard_hist_freq=tensorboard_hist_freq)
+        test_scores = self.evaluate(image_labels_path=test_paths_labels, norm=norm, _resize=_resize, verbose=verbose)
+        val_scores = self.evaluate(image_labels_path=val_paths_labels, norm=norm, _resize=_resize, verbose=verbose)
+
+        if eval_table is not None:
+            eval_table.add_row(
+                [prefix, f'{train_time:.2f}'] + [test_scores[0]] + [f'{s * 100:.2f}%' for s in test_scores[1:]]
+                + [val_scores[0]] + [f'{s * 100:.2f}%' for s in val_scores[1:]])
+            print(eval_table)
+
+        return val_scores[1]
--- utils.py
+++ utils.py
@@ -4,7 +4,10 @@ import numpy as np
 import pandas as pd
 from string import ascii_lowercase, ascii_uppercase
 import itertools
+from tensorflow.keras import backend as BE
+import tensorflow as tf
 import IPython.display as display
+
 TRAIN_FILEPATH = 'pairsDevTrain.txt'
 TEST_FILEPATH = 'pairsDevTest.txt'
 SOURCE_PATH = 'lfw2/lfw2'
@@ -12,6 +15,10 @@ TRAIN_DST_PATH = 'data/train/'
 TEST_DST_PATH = 'data/test/'
 UNUSED_DST_PATH = 'data/etc/'
 
+
+# -----------------------------------------------------------
+#                  PREPROCESSING UTILS
+# -----------------------------------------------------------
 
 def _create_pairs(textfile_path):
     """
@@ -41,7 +48,7 @@ def _flatten_dirs(src, verbose=False):
     :param verbose: True if we want logging of the moving, else False.
     """
     for dir in os.listdir(src):
-        for file in os.listdir(os.path.join(src,dir)):
+        for file in os.listdir(os.path.join(src, dir)):
             if verbose:
                 print("Moving file: " + file + "....")
             shutil.move(os.path.join(src, dir, file), os.path.join(src, file))
@@ -83,10 +90,6 @@ def prepare_data(src_path=SOURCE_PATH, train_path=TRAIN_DST_PATH,
     shutil.move(src_path, unused_path)
 
 
-def get_image_name(path):
-    return path.split('/')[-1].split('.')[0]
-
-
 def print_data_statistics(label_paths, data_name='train', label_name=['Same', 'Different']):
     """
     print statistics about about the labels of the given path
@@ -95,25 +98,6 @@ def print_data_statistics(label_paths, data_name='train', label_name=['Same', 'D
     :param label_name: kind of labels.
     :return:
     """
-    print('Examples for {} corpus structure:'.format(data_name))
-    print('############################')
-    print('View:')
-    print(label_paths[:3])
-    print('############################')
-    print('Corpus size: {}'.format(len(label_paths)))
-    print('Number of classes: {}'.format(len(label_name)))
-    labels = np.array([label for _, _, label in label_paths])
-    label_row_list = []
-    for index, name in enumerate(label_name):
-        name = name
-        size = len(labels[labels==index])/len(label_paths)
-        percentage = f'{size*100:.2f}%'
-        label_row_list.append((name, len(labels[labels==index]), len(label_paths), percentage))
-    df = pd.DataFrame(label_row_list, columns=['Class', 'Number in Class', 'Total', 'Percentage'])
-    print(df)
-
-
-def print_data_statistics(label_paths, data_name='train', label_name=['Same', 'Different']):
     print('Examples for {} corpus structure:'.format(data_name))
     print('############################')
     print('View:')
@@ -184,3 +168,177 @@ def split_data_by_letters(labels, letters):
         else:
             val.append(paths)
     return train, val
+
+
+# -----------------------------------------------------------
+#                  DISTANCE UTILS
+# -----------------------------------------------------------
+
+
+def abs_distance(tensors, K=BE):
+    """
+    calculates the absolute distance between two tensor
+    :param tensors: 2 tensors
+    :return: the absolute distance
+    """
+    return BE.abs(tensors[0] - tensors[1])
+
+
+def l2_distance(tensors):
+    """
+    calculates euclidiean distance
+    :param tensors: 2 tensors
+    :return: the euclidean distance
+    """
+    return BE.sqrt(BE.sum(BE.square(abs_distance(tensors)), axis=1, keepdims=True))
+
+
+def l2_distance_shape(shapes):
+    """ gets tuple of shape of vectors and return the shape of the tensor"""
+    shape1, shape2 = shapes
+    return shape1[0], 1
+
+
+def distance_acc(y_true, y_pred, th=0.5):
+    """
+    calculates the accuracy of the classification with threshold on distances
+    :param y_true: the expected labels
+    :param y_pred: the predicted labels
+    :param th: th of the distance to be classified as 0 or 1
+    :return:
+    """
+    return BE.mean(BE.equal(y_true, BE.cast(y_pred < th, y_true.dtype)))
+
+
+# -----------------------------------------------------------
+#                  PREPROCESSING UTILS
+# -----------------------------------------------------------
+
+
+def image2tensor(img_raw, norm=None, _resize=None):
+    """
+    preprocessing raw image to tensor (using libraries from tensorlow)
+    :param img_raw: the raw image
+    :param norm: value to normalize the image (like 255.0)
+    :param resize: the shape we need to be resize to the image
+    :return: the image after the preprocessing as a tensor
+    """
+
+    img_tensor = tf.image.decode_jpeg(img_raw, channels=1)
+    if _resize is not None:
+        img_tensor = tf.image.resize(img_tensor, _resize)
+    if norm is not None:
+        img_tensor = tf.cast(img_tensor, tf.float32) / norm
+    return img_tensor
+
+
+def load_image_as_tensor(img_path, norm=None, _resize=None):
+    """
+    load image and then preprocess it to tensor
+    :param img_path: the path of the image
+    :param norm: value to normalize the image (like 255.0)
+    :param _resize: the shape we need to be resize to the image
+    :return: the image after the preprocessing as a tensor
+    """
+    img_raw = tf.io.read_file(img_path)
+    return image2tensor(img_raw, norm=norm, _resize=_resize)
+
+
+def load_images_as_tensor_with_label(paths, label, norm=None, _resize=None):
+    """
+    load left and right image (with their label) as a tensor
+    :param paths: the path of the left and right imaages
+    :param label: the label of the image (y-true)
+    :param norm: value to normalize the image (like 255.0)
+    :param _resize: the shape we need to be resize to the image
+    :return: 3-tuple of left image, right image and their label
+    """
+    left_img = load_image_as_tensor(paths[0], norm=norm, _resize=_resize)
+    right_img = load_image_as_tensor(paths[1], norm=norm, _resize=_resize)
+    return left_img, right_img, label
+
+
+# -----------------------------------------------------------
+#                  CREATING TENSOR DATASET UTILS
+# -----------------------------------------------------------
+
+def create_tensor_dataset(img_paths=None, labels=None, norm=None, _resize=None):
+    """
+    create the tensor data (full dataset, image dataset or only label dataset) to the form of tensor
+    :param img_paths: the paths of the images
+    :param labels: the labels of the images
+    :param norm: value to normalize the image (like 255.0)
+    :param _resize: the shape we need to be resize to the image
+    :return: the tensor dataset
+    """
+    tensor_data = None
+    if img_paths is not None and labels is not None:
+        tensor_data = tf.data.Dataset.from_tensor_slices((img_paths, tf.cast(labels, tf.bool)))
+        tensor_data = tensor_data.map(lambda paths, label: load_images_as_tensor_with_label(paths, label, norm=norm,
+                                                                                            _resize=_resize),
+                                      num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    elif img_paths is not None:  # image dataset
+        tensor_data = tf.data.Dataset.from_tensor_slices(img_paths)
+        tensor_data = tensor_data.map(lambda paths: load_image_as_tensor(paths, norm=norm, _resize=_resize),
+                                      num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    elif labels is not None:  # label dataset
+        tensor_data = tf.data.Dataset.from_tensor_slices(tf.cast(labels, tf.bool))
+    return tensor_data
+
+
+def prepare_tensor_dataset(tensor_data, batch_size, buffer_size):
+    """
+    preparing the tensor so that the shuffle buffer size as large as the dataset to ensure that the data is completely
+    shuffled
+    :param tensor_data: the dataset
+    :param batch_size:  the size of the batch
+    :param buffer_size: the size of the buffer
+    :return: the dataset after prefetching and shuffle
+    """
+    tensor_data = tensor_data.shuffle(buffer_size=buffer_size)
+    tensor_data = tensor_data.repeat()
+    tensor_data = tensor_data.batch(batch_size=batch_size)
+    tensor_data = tensor_data.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+    return tensor_data
+
+
+def init_tensor_data(tensor_data, images_labels_path, norm=255.0, _resize=[256, 256], batch_size=1, buffer_size=None,
+                     verbose=True):
+    """
+    initializing the tensor dataset
+    :param tensor_data: the dataset
+    :param images_labels_path: the images and labels path
+    :param norm: value to normalize the image (like 255.0)
+    :param _resize: the shape we need to be resize to the image
+    :param batch_size: the batch size of the tensor
+    :param buffer_size: the buffer size for shuffling
+    :param verbose:
+    :return:
+    """
+    if tensor_data is None:
+        if buffer_size is None:
+            buffer_size = len(images_labels_path)
+        images_paths = [(p[0], p[1]) for p in images_labels_path]
+        labels = [ p[2] for p in images_labels_path]
+        tensor_data = create_tensor_dataset(img_paths=images_paths, labels=labels, norm=norm, _resize=_resize)
+        tensor_data = prepare_tensor_dataset(tensor_data, batch_size=batch_size, buffer_size=buffer_size)
+
+        if verbose:
+            print(tensor_data)
+
+    return tensor_data
+
+
+def split_train_test_tensor(tensor_data, data_len, test_size=0.2):
+    """
+    split the tensor to train and test tensor
+    :param tensor_data:
+    :param data_len:
+    :param test_size:
+    :return:
+    """
+    test_data_len = int(test_size * data_len)
+    test_data = tensor_data.take(test_data_len)
+    train_data = tensor_data.skip(test_data_len)
+    return train_data, test_data
+
