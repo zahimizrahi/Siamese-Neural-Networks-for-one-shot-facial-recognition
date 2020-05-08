5b420fdab6a8c131aeebed17482c36f9f19543fc
zahimizrahi <zahimizrahi1@gmail.com>
2020-05-08 19:53:44 +0300

    Added documentation

--- utils.py
+++ utils.py
@@ -1,10 +1,23 @@
 from string import ascii_lowercase, ascii_uppercase
 import itertools
 
+
 def get_image_name(path):
+    """
+    get the image_name from full path of image
+    :param path:
+    :return: image name
+    """
     return path.split('/')[-1].split('.')[0]
 
+
 def get_histogram_of_letters(people_names, num_letters=1):
+    """
+    shows histogram of all the letters
+    :param people_names:
+    :param num_letters:
+    :return: histogram of all the people names by their first letter in the name
+    """
     combination = list(ascii_uppercase)
     lower = ascii_lowercase
     if num_letters > 1:
@@ -15,7 +28,18 @@ def get_histogram_of_letters(people_names, num_letters=1):
         hist[elem] = sum(p.startswith(elem) for p in people_names)
     return hist
 
+
 def subset_sum(numbers, desired_length, target, delta, partial=[], result=[]):
+    """
+    subset sum algorithms
+    :param numbers:
+    :param desired_length:
+    :param target:
+    :param delta:
+    :param partial:
+    :param result:
+    :return: the combination (subset)
+    """
     s = sum(partial)
 
     # check if the partial sum is equals to target
@@ -30,11 +54,13 @@ def subset_sum(numbers, desired_length, target, delta, partial=[], result=[]):
         subset_sum(remaining, desired_length, target, delta, partial + [n])
     return result
 
+
 def split_data_by_letters(labels, letters):
     """
-    :param labels:
-    :param letters:
-    :return:
+    split the labels the given letters
+    :param labels: the labels to be splitted
+    :param letters: the letters which indicates how to split the labels
+    :return: 2 lists, the  first contains all the labels without the letters and the second with the letters.
     """
     train = []
     val = []
@@ -50,6 +76,7 @@ def split_data_by_letters(labels, letters):
         else:
             val.append(paths)
     return train, val
+
 
 # CHECK IF NEED IT
 def split_train_test_tensor(tensor_data, data_len, test_size=0.2):
